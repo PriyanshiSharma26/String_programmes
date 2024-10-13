1c444bbb3d7573da28e12d792c024c6c7d1d223f
PriyanshiSharma26 <priyanshisharma8220708@gmail.com>
2024-10-13 09:31:44 -0700

    Add files via upload

---
+++ _12_consonent_counter.java
@@ -0,0 +1,19 @@
+ class ConsonantCounter {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                if (!(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                      c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Number of consonants: " + count);
+    }
+}
+
